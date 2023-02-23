7bf11ec9d6983e18590646ce60ec0146f63fb70e
Nabaneet247 <nabaneetroy88@gmail.com>
2023-02-23 10:30:28 +0530

    env changes

---
+++ env.js
@@ -0,0 +1,7 @@
+(function (window) {
+    window.__env = window.__env || {};
+
+    // API url
+    window.__env.apiUrl = "http://ec2-43-204-240-96.ap-south-1.compute.amazonaws.com/api";
+    window.__env.logoutRedirectUrl = "https://www.google.co.in";
+}(this));
