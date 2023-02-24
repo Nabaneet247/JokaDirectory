3aa0f72f850da5860e0cf93a9556e6bbcb78f46c
Nabaneet247 <nabaneetroy88@gmail.com>
2023-02-24 21:20:38 +0530

    fix again

--- src/components/jd-components.module.js
+++ src/components/jd-components.module.js
@@ -1,3 +1,4 @@
 import search from "./search/search.component.js";
+import viewUser from "./view-user/view-user.component.js"
 
-angular.module("jd-components", ["search"]);
+angular.module("jd-components", ["search", "view-user"]);
