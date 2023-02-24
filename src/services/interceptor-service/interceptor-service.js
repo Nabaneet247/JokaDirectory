1f61f335f8cc62cc8e8595fa3f221dcb42caafab
Nabaneet247 <nabaneetroy88@gmail.com>
2023-02-24 22:16:29 +0530

    temporary fix

--- src/services/interceptor-service/interceptor-service.js
+++ src/services/interceptor-service/interceptor-service.js
@@ -1,28 +1,30 @@
-  export default class InterceptorService {
-    constructor(configData) {
-      this.loginPageRedirectUrl = configData.loginPageRedirectUrl;
-    }
-
-    request = (config) => {
-      if (!config.url.includes("/api/jd")) return config;
-      config.headers = {
-        ...config.headers,
-        joka_auth_token: window.localStorage.getItem("joka_auth_token"),
-      };
-      return config;
-    }
-
-    response = (res) => {
-      if (!res.config.url.includes("/api/jd")) return res;
-
-      window.localStorage.setItem("joka_auth_token", res.headers("joka_auth_token"));
-      return res;
-    }
-
-    responseError = (error) => {
-      if (error.status == 401) {
-        window.open(`${this.loginPageRedirectUrl}${window.location.href}`, "_self");
-      }
-      return error;
-    }
+export default class InterceptorService {
+  constructor(configData) {
+    this.loginPageRedirectUrl = configData.loginPageRedirectUrl;
   }
+
+  request = (config) => {
+    if (!config.url.includes("/api/jd")) return config;
+    config.headers = {
+      ...config.headers,
+      joka_auth_token: window.localStorage.getItem("joka_auth_token"),
+    };
+    return config;
+  };
+
+  response = (res) => {
+    if (!res.config.url.includes("/api/jd")) return res;
+
+    if (res.headers("joka_auth_token")) {
+      window.localStorage.setItem("joka_auth_token", res.headers("joka_auth_token"));
+    }
+    return res;
+  };
+
+  responseError = (error) => {
+    if (error.status == 401) {
+      window.open(`${this.loginPageRedirectUrl}${window.location.href}`, "_self");
+    }
+    return error;
+  };
+}
