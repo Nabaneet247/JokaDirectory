f8fafa1f2793348138a659afae45c9fcf13e30d0
Nabaneet247 <nabaneetroy88@gmail.com>
2023-03-07 19:27:52 +0530

    t

--- src/app/services/backend-service/backend-service.js
+++ src/app/services/backend-service/backend-service.js
@@ -56,7 +56,7 @@ export default class BackendService {
         method: 'POST',
         url: this.apiUrl + "/jd/saveUserImage",
         data: form_data,
-        headers: { 'Content-Type': 'multipart/form-data'},});
+        headers: { 'Content-Type': undefined},});
       console.log(res);
       return true;
     } catch (err) {
